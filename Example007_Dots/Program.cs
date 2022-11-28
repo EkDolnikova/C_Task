1f6a9766f30f6bc33bdf341a8eddc3d1755c1f96
Dolnikova <ek.dolnikowa@yandex.ru>
2022-11-29 00:26:44 +0300

    добавили работу цикла

---
+++ Example007_Dots/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Clear(); //очистка консоли
+Console.SetCursorPosition(10, 4);
+Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
+
+
+Console.SetCursorPosition(xa, ya); //устанавливаем точку ха и ya
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb); //устанавливаем точку ха и ya
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc); //устанавливаем точку ха и ya
+Console.WriteLine("+");
+
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 100);
+{
+    int what = new Random().Next(0, 3);
+    if(what == 0)
+    {
+        x = (x + xa)/2;
+        y = (y + ya)/2;
+    }
+
+    if(what == 1)
+    {
+        x = (x + xb)/2;
+        y = (y + yb)/2;      
+    }
+
+    if(what == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1;
+
+}
+
